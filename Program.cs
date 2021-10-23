dd843a66fd6944bb70123d225615920557dd9a8a
Ledle <trojangoblin@gmail.com>
2021-10-23 14:41:02 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgLab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> ss = new List<String>();
+            ss.Add("Hello");
+            ss.Add("World");
+            Console.WriteLine(ss[0] + ss[1] + ss.Count);
+        }
+    }
+}
