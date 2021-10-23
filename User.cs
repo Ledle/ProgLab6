0bcb8517fda04155505c774653299e1c217e376f
Ledle <trojangoblin@gmail.com>
2021-10-23 16:18:17 +0700

    Added user

--- User.cs
+++ User.cs
@@ -8,6 +8,77 @@ namespace ProgLab6
 {
     class User
     {
-
+        private int login, password;
+        private String name;
+        private Group grp;
+        public int Login{
+            get { return login; }
+            set { login = value; }
+        }
+        public int Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Group Group {
+            get { return grp; }
+        }
+        public User()
+        {
+            this.name = "";
+            this.grp = null;
+            this.login = 0;
+            this.password = 0;
+        }
+        public User(int login, int password, String name)
+        {
+            this.login = login;
+            this.password = password;
+            this.name = name;
+            this.grp = null;
+        }
+        public void changegroup(Group gr)
+        {
+            if (grp != gr)
+            {
+                //gr.adduser(this);
+                if (this.grp != null)
+                {
+                    //this.grp.deluser(this.login);
+                }
+                this.grp = gr;
+            }
+        }
+        public void delgroup()
+        {
+            if (this.grp != null)
+            {
+                //this.grp.deluser(this.login);
+            }
+            this.grp = null;
+        }
+        public void show()
+        {
+            Console.WriteLine("User: " + this.name);
+            if (this.grp != null)
+            {
+                //Console.WriteLine(" Group: " + this.grp.getname());
+            }
+            Console.WriteLine(" Login: " + this.login + " Password: " + this.password);
+        }
+        public void input()
+        {
+            Console.Write("Enter name of student: ");
+            this.name = Console.ReadLine();
+            Console.Write("Enter login: ");
+            this.login = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter password: ");
+            this.password = Convert.ToInt32(Console.ReadLine());
+        }
     }
 }
