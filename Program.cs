67f0ffdd369faa28b5202ba9b66683e60fdc95b7
Ledle <trojangoblin@gmail.com>
2021-11-24 23:02:15 +0700

    ref out

--- Program.cs
+++ Program.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 
 namespace ProgLab6
 { 
-    class Program
-    {
+    class Program {
+        static void change(int a, out int b, ref int c) 
+        {
+            a = 3;
+            b = 4;
+            c = 5;
+        }
         static void Main(string[] args)
         {
-            Question q = new Question();
-            q.Value = 12;
-            q.show();
-            (q++).show();
-            q.show();
-            (++q).show();
-            q += 10;
-            q.show();
+            int a = 0,c = 1;
+            change(a, out int b, ref c);//b объявляется в 
+            Console.WriteLine($"a:{a} b:{b} c:{c}");
 		}
     }
 }
